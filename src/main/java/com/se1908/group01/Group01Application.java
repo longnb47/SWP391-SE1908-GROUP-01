391178cf43d9b240635bf665baceeb10e271d426
longnb47 <long.nb47@gmail.com>
2026-05-30 20:44:33 +0700

    init project spring boot

---
+++ src/main/java/com/se1908/group01/Group01Application.java
@@ -0,0 +1,13 @@
+package com.se1908.group01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/se1908/group01/Group01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se1908.group01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
