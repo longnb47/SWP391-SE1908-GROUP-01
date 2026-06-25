fe2bfa81ff05a80a91dce8aef13bd3d94314b7a8
DanhCao2703 <danhcao270305@gmail.com>
2026-06-25 12:39:09 +0700

    add SubscriptionPlanController

---
+++ src/main/java/com/se1908/group01/controller/SubscriptionPlanController.java
@@ -0,0 +1,71 @@
+package com.se1908.group01.controller;
+
+import com.se1908.group01.dto.CreatePlanRequest;
+import com.se1908.group01.dto.UpdatePlanRequest;
+import com.se1908.group01.entity.SubscriptionPlan;
+import com.se1908.group01.service.SubscriptionPlanService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/subscription-plans")
+@RequiredArgsConstructor
+@SecurityRequirement(name = "bearerAuth")
+public class SubscriptionPlanController {
+
+
+    private final SubscriptionPlanService service;
+
+    @PostMapping
+    public ResponseEntity<SubscriptionPlan> create(
+            @RequestBody CreatePlanRequest request) {
+
+        return ResponseEntity.ok(
+                service.create(request)
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SubscriptionPlan>> getAll() {
+
+        return ResponseEntity.ok(
+                service.getAll()
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SubscriptionPlan> getById(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                service.getById(id)
+        );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SubscriptionPlan> update(
+            @PathVariable Long id,
+            @RequestBody UpdatePlanRequest request) {
+
+        return ResponseEntity.ok(
+                service.update(id, request)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(
+            @PathVariable Long id) {
+
+        service.delete(id);
+
+        return ResponseEntity.ok(
+                "Subscription plan deleted successfully"
+        );
+    }
+
+
+}
